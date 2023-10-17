882262e635c314878ca86906bb6948ff1141bcb8
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2023-10-17 18:43:27 -0300

    ajustei códigos

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 function tocaSomAplausos(){
     document.querySelector("#som_tecla_aplausos").play();
     }
-/*function tocaSomVaia(){
-    document.querySelector("#som_tecla_vaia").play();
-    }*/
 
 document.querySelector(".tecla_aplausos").onclick = tocaSomAplausos;   
 
-//const listaDeTeclas = document.querySelectorAll(".tecla");
-//listaDeTeclas[0].onclick = tocaSomAplausos;
-//listaDeTeclas[1].onclick = tocaSomVaia;
