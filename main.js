210a286775ed017438b8f3f18a2ff7008c98c04c
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2023-10-23 08:34:42 -0300

    atualizei o main.js

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-function tocaSomAplausos(){
+/*function tocaSomAplausos(){
     document.querySelector("#som_tecla_aplausos").play();
     }
 
-document.querySelector(".tecla_aplausos").onclick = tocaSomAplausos;   
+document.querySelector(".tecla_aplausos").onclick = tocaSomAplausos; */  
 
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function (){
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+}
