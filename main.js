7d22f3d97b5bdd06496663866f8198c081357c76
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2023-11-07 19:22:23 -0300

    Atualizei o main.js

--- main.js
+++ main.js
@@ -1,16 +1,95 @@
-function tocaSomAplausos(){
-    document.querySelector("#som_tecla_aplausos").play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio);
     }
-function tocaSomVaia(){
+    contador = contador + 1;
+   
+}
+
+
+/*for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function tocaSomVaia(){
+        document.querySelector("#som_tecla_vaia").play();
+        }*/
+    
+
+
+//listaDeTeclas[1].onclick = tocaSomVaia; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//listaDeTeclas[1].onclick = tocaSomVaia;
+
+
+
+/*function tocaSomVaia(){
     document.querySelector("#som_tecla_vaia").play();
-    }
-function tocaSomRisada(){
-    document.querySelector("#som_tecla_risada").play();
-
-    }                    
-
-const listaDeTeclas = document.querySelectorAll(".tecla");   
-listaDeTeclas[0].onclick = tocaSomAplausos;
-listaDeTeclas[1].onclick = tocaSomVaia;
-listaDeTeclas[2].onclick = tocaSomRisada;
-
+    }*/
