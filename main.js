ceea5a0a427339d06f3cbe21f904dffcca7ccde3
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2023-11-29 17:05:50 -0300

    Atualizei o main.js

--- main.js
+++ main.js
@@ -1,19 +1,73 @@
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
+    
 }
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
 
-while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas[contador].classList[1];
-    const idAudio = "#som_"+efeito;
-    listaDeTeclas[contador].onclick = function(){
+
+
+
+/*function tocaSom(idElementoAudio){
+
+    const elemento = document.querySelector(idElementoAudio);
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }else{
+        console.log("Elemento não encontrado ou seletor inválido");
+    }
+}*/
+
+
+
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+
+    tecla.onclick = function (){
         tocaSom(idAudio);
     }
-    contador = contador + 1;
-   
+    tecla.onkeydown = function(evento){
+        if (evento.code === 'Space'){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
 
 
 
